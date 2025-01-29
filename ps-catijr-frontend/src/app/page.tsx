bbaa178501edb1205cbbc41cf382f78ce058ecf0
Lucas Sciarra <lucas.goncalves@vonbraunlabs.com.br>
2025-01-29 15:26:41 -0300

    feat: scroll horizontal

--- ps-catijr-frontend/src/app/page.tsx
+++ ps-catijr-frontend/src/app/page.tsx
@@ -9,35 +9,50 @@ import useSWR from 'swr'
 const fetcher = (url:string) => fetch(url).then((res) => res.json());
 
 export default function Home() {
-  //Fetch lists
-  const { data, error } = useSWR('http://localhost:3333/lists', fetcher)
+  // Fetch lists
+  const { data, error } = useSWR('http://localhost:3333/lists', fetcher);
 
-  //Fetch tasks
+  // Fetch tasks
   const { data: tasks, isLoading } = useSWR(`http://localhost:3333/tasks`, fetcher);
 
+  if (error) return <div>failed to load</div>;
+  if (!data || isLoading) return <div>loading...</div>;
 
-  if (error) return <div>failed to load</div>
-  if (!data || isLoading) return <div>loading...</div>
-
-  console.log(data)
-  console.log(tasks)
+  console.log(data);
+  console.log(tasks);
 
   return (
-    <div className="flex gap-2 text-white px-[80px] ">
-      {data.map((list:any, index:number) => (
-        <div key={index} className="flex flex-col gap-4 border border-[#4E4E4E] pt-[16px] px-[16px] pb-[24px] rounded-md">
-          <ListTitle key={list.id} listId={list.id} listTitle={list.title}></ListTitle>
+    <div className="px-[80px] text-white">
+      <div className="flex gap-2 overflow-x-auto scrollbar-thin scrollbar-thumb-gray-600 scrollbar-track-gray-800">
+        {data.map((list: any, index: number) => (
+          <div
+            key={index}
+            className="flex flex-col gap-4 border border-[#4E4E4E] pt-[16px] px-[16px] pb-[24px] rounded-md"
+          >
+            <ListTitle key={list.id} listId={list.id} listTitle={list.title}></ListTitle>
 
-          {tasks.filter((task:any) => task.listId === list.id).map((task:any) => (
-            <TaskCard key={task.id} date={task.finishAt <= Date.now() ? "onTime" : "late"} 
-                      priority={task.priority} title={task.title} description={task.description} 
-                      taskDate={task.finishAt} isFinished={task.finishedAt} id={task.id} listId={list.id} ></TaskCard>
-          ))}
+            {tasks
+              .filter((task: any) => task.listId === list.id)
+              .map((task: any) => (
+                <TaskCard
+                  key={task.id}
+                  date={task.finishAt <= Date.now() ? "onTime" : "late"}
+                  priority={task.priority}
+                  title={task.title}
+                  description={task.description}
+                  taskDate={task.finishAt}
+                  isFinished={task.finishedAt}
+                  id={task.id}
+                  listId={list.id}
+                ></TaskCard>
+              ))}
 
-          <NewTaskButton listId={list.id}></NewTaskButton>
-        </div>
-      ))}
-      <NewListButton></NewListButton>
+            <NewTaskButton listId={list.id}></NewTaskButton>
+          </div>
+        ))}
+        <NewListButton></NewListButton>
+      </div>
     </div>
   );
 }
+
